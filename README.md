7d7e3f9e820cba984431855774f0e3fc9c20b66e
N Manning <nmanning4@outlook.com>
2025-07-27 16:45:55 -0400

    7-27-25: Final Calculator

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # General-Inquiry-JS-Calculator
+
+# Welcome to Nia Manning's Calculator!!! 
+
+Before you begin reading... I hope you enjoyed the nice pun across the top of the page!! 😁
+
+This simple calculator performs similarly to a small handheld calculator. The user can use this simple calculator to complete math problems using addition, subtraction, multiplication, and division.
+The Calculator also includes functions for percentages, pi, and includes a button where the user can toggle the positive/negative sign of the displayed number.
+This calculator can be controled by using the mouse to select the numbers needed for the user's desired equation.
+
+What Are the Functions on the calculator?
+1. Delete/Backspace Button (DEL)
+2. All Clear (AC)
+3. Division (/)
+4. Multiplication (x)
+5. Addition (+)
+6. Subtraction (-)
+7. Calcualte (=)
+8. Plus/Minus (+/-)
+9. Percentage (%)
+10. Pi (π)
+
+How to use?
+1. The user must use the mouse to click the buttons to achieve necessary results. 
+2. Once you find the solution to the problem, the user can do the following: 
+    a. Use the AC or DEL button to input a new math problem.
+    b. Use the solution from the previous math problem to create a new equation
+
+# Math Problem Examples: 
+** [] : brackets indicate which button to click **
+
+Calculations:
+1. 8 + 9 = 17   >>>   8 [+] 9 [=]
+2. 8 - 9 = -1   >>>    8 [-] 9 [=]
+3. 8 * 9 = 72   >>>    8 [*] 9 [=]
+4. 8 / 9 = 0.88 >>>  8 [/] 9 [=]
+5. 8 +/- = -8   >>>  8 [+/-]
+6. 8 % = 0.08   >>>    8 [%]
+7. 8 π = 25.132741228718345  >>> 8 [π]
+9. AC  >>> Clears User Input
+10. DEL >>> Backspaces Input
+
+# Errors
+Should the user encounter an error.
+1. User has to select #'s 0-9 before selecting the pos/neg, pi, equal sign, and percentage buttons.
+2. User can not divide a number by 0.
--- calc.js
+++ calc.js
@@ -1,25 +1,36 @@
 /*Nia Manning
  7-28-2025
- Guided Inquiry #7 */
+ Guided Inquiry #7*/
 const calcDisplay = document.getElementById("calcDisplay");
 
 function appendToDisplay(input){ /* Puts calculations on the screen */
     calcDisplay.value += input;
     console.log("input pressed");
  }
-
- function clearDisplay(){ /*Clear Numbers from Calculator*/
+ function clearDisplay(){ /* AC Button. Clear Numbers from Calculator*/
     calcDisplay.value = "";
     console.log("input cleared");
  }
-
- function changeSign(){ //+/- button
-    let newNum = parseFloat(calcDisplay.value); //parses a string argument and returns a floating point number.
-    newNum = -newNum;
-    calcDisplay.value = newNum;
-    console.log("is it positive? or negative?")
+ function dltNum(){
+    calcDisplay.value = calcDisplay.value.slice(0,-1);
+    console.log("backspace");
  }
-
+ /* +/- button
+ changes numerical value to a positive or negative number*/ 
+ function changeSign(){ 
+    let newNum = parseFloat(calcDisplay.value); //parses a string argument and returns a floating point number.
+    if(!isNaN(newNum)){
+         newNum = -newNum;
+        calcDisplay.value = newNum;
+        console.log("is it positive? or negative?")
+    }else{
+        calcDisplay.value = 'Error';
+        alert('Invalid input! Please click buttons 0-9.');
+        console.log("invalid input");
+    }
+ }
+ /* % button
+ divides user input by 100*/ 
   function percentDec(){ /* % button*/
     let newNum = parseFloat(calcDisplay.value);
     if(!isNaN(newNum)){
@@ -28,26 +39,46 @@ function appendToDisplay(input){ /* Puts calculations on the screen */
         console.log("percentages.");
     }else{
         calcDisplay.value = 'Error';
-        alert('Invalid input!');
+        alert('Invalid input! Please click buttons 0-9.');
+        console.log("invalid input");
     }
  }
-
- function piCalc(){ //pi button
+  /* pi button
+multiplies user input by Pi.*/ 
+ function piCalc(){ 
     let newPiNum = parseFloat(calcDisplay.value);
     if(!isNaN(newPiNum)){
         let resultPi = newPiNum * Math.PI;
         calcDisplay.value = resultPi;
         console.log("pi time");
     }else{
-        alert('Invalid input!');
+        calcDisplay.value = 'Error';
+        alert('Invalid input! Please click buttons 0-9.');
+        console.log("invalid input");
     }
  }
- 
- function calculate(){ //try...catch statement
+// try...catch statement to calculate user input
+ function calculate(){ 
+    const input = calcDisplay.value;
     try {
-        calcDisplay.value = eval(calcDisplay.value);
-        console.log("equals");
+        /* Prints out an error alert instead of infinity when user divides a whole number by 0.*/
+        if (input.includes("/0")){
+            calcDisplay.value = 'Error';
+            alert('Invalid input! Number can not be divided by 0.');
+            console.log("cannot divide by 0.");
+        } else{
+            calcDisplay.value = eval(calcDisplay.value);
+            console.log("equals");
+        }
+    /* Prints out an error alert if user clicks equal button with no input*/
+        if(input.trim() === ""){
+            calcDisplay.value = 'Error';
+            alert('Invalid input! Please click buttons 0-9.');
+            console.log("equals");
+        }
+    /* Prints out an error alert in case something is misclicked*/
     } catch (error) {
-        alert('Invalid input!');
+        alert('Invalid input! Please click buttons 0-9.');
+        console.log("invalid input");
     }
  }
